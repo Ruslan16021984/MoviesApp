d24ef96c4c4bdc484aadace00ceeabe4d9603776
anenkov <anenkov@magora-systems.com>
2021-10-06 18:23:36 +0300

    tv_show fragment xml created

---
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowItem.kt
@@ -0,0 +1,20 @@
+package ru.androidschool.intensiv.ui.tvshows
+
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+import com.xwray.groupie.kotlinandroidextensions.Item
+import kotlinx.android.synthetic.main.item_tv_show.*
+import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.data.TvShow
+
+class TvShowItem(private val content: TvShow): Item() {
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        viewHolder.description_tv.text = content.title
+        viewHolder.movie_rating_tv.rating = content.rating
+        Picasso.get()
+            .load("https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg")
+            .into(viewHolder.image_preview_show)
+    }
+
+    override fun getLayout()=R.layout.item_tv_show
+}
