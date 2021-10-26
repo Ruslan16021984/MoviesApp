9382ecfa5a1106cc6ba0fcf3f33cac10d63976c0
anenkov <anenkov@magora-systems.com>
2021-10-26 20:44:32 +0300

    home_work_5

--- app/src/main/java/ru/androidschool/intensiv/ui/feed/FeedFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/feed/FeedFragment.kt
@@ -11,21 +11,19 @@ import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import io.reactivex.Observable
 import io.reactivex.ObservableOnSubscribe
-import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.feed_fragment.*
 import kotlinx.android.synthetic.main.feed_header.*
 import kotlinx.android.synthetic.main.search_toolbar.view.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.data.movie.MovieData
 import ru.androidschool.intensiv.data.movie.MovieResponse
 import ru.androidschool.intensiv.data.movie.Movies
 import ru.androidschool.intensiv.network.MovieApiClient
 import ru.androidschool.intensiv.ui.afterTextChanged
-import ru.androidschool.intensiv.ui.tvshows.TvShowItem
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
 
@@ -43,93 +41,60 @@ class FeedFragment : Fragment(R.layout.feed_fragment) {
         }
     }
 
-    @SuppressLint("CheckResult")
+    @SuppressLint("CheckResult", "LogNotTimber")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        Observable.create(ObservableOnSubscribe<String> {
-            search_toolbar.search_edit_text.afterTextChanged { text ->
-                it.onNext(text.toString())
+        search_toolbar.search_edit_text.afterTextChanged {
+            Timber.d(it.toString())
+            if (it.toString().length > MIN_LENGTH) {
+                openSearch(it.toString())
             }
-        }).map { it.trim() }
-            .debounce(500, TimeUnit.MILLISECONDS)
-            .filter { it.length > 2 }
-            .observeOn(Schedulers.io())
-            .flatMapSingle { MovieApiClient.apiClient.searchByQuery("ru", it) }
-            .observeOn(AndroidSchedulers.mainThread()).subscribe (
-                {setMovies(it)},
-                {
-                    Log.e("TAG", "Error: ${it.message}" )
-                }
-            )
-//        search_toolbar.search_edit_text.afterTextChanged {
-//            Timber.d(it.toString())
-//            if (it.toString().length > MIN_LENGTH) {
-//                openSearch(it.toString())
-//            }
-//        }
+        }
         val getTopRatedMovies = MovieApiClient.apiClient.getUpcomingMovies("ru", 1)
         val getPopularMovie = MovieApiClient.apiClient.getPopularMovie("ru", 1)
-        getTopRatedMovies.enqueue(object : Callback<MovieResponse> {
-            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
-                // Передаем результат в adapter и отображаем элементы
-                val moviesList = listOf(
-                    response.body()?.results?.map {
+        Single.zip(
+            getTopRatedMovies,
+            getPopularMovie,
+            BiFunction<MovieResponse, MovieResponse, MovieData> { source1, source2 ->
+                val topRatedMoviesList = listOf(
+                    source1.results.map {
                         MovieItem(it) { movie ->
                             openMovieDetails(
                                 movie
                             )
                         }
-                    }?.let {
+                    }.let {
                         MainCardContainer(
                             R.string.recommended,
                             it.toList()
                         )
                     }
                 )
-                adapter.apply { addAll(moviesList) }
-            }
-
-            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-                Log.e("TAG", "onFailure: ${t.message}")
-            }
-
-        })
-        getPopularMovie.enqueue(object : Callback<MovieResponse> {
-            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
-                val moviesList = listOf(
-                    response.body()?.results?.map {
+                val getPopularList = listOf(
+                    source2.results.map {
                         MovieItem(it) { movie ->
                             openMovieDetails(
                                 movie
                             )
                         }
-                    }?.let {
+                    }.let {
                         MainCardContainer(
                             R.string.upcoming,
                             it.toList()
                         )
                     }
                 )
-                adapter.apply { addAll(moviesList) }
-            }
-
-            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-                Log.e("TAG", "onFailure: ${t.message}")
-            }
-
-
+                MovieData(sourceTopRated = topRatedMoviesList, sourceGetPopular = getPopularList)
+            }).subscribeOn(Schedulers.io())
+            .doOnSubscribe { progress_bar.visibility = View.VISIBLE }
+            .doFinally {progress_bar.visibility = View.INVISIBLE  }
+            .observeOn(AndroidSchedulers.mainThread()).subscribe({
+            adapter.apply { addAll(it.sourceTopRated) }
+            adapter.apply { addAll(it.sourceGetPopular) }
+        }, {
+            Log.e("TAG", "${it.message} " )
         })
-
         movies_recycler_view.adapter = adapter
-    }
-    private fun setMovies(movieResponse: MovieResponse){
-        val movieList = movieResponse.results.map { MovieSearchItem(it){ movie ->
-            openMovieDetails(
-                movie
-            )
-        } }
-        movies_recycler_view.adapter = adapter.apply { addAll(movieList) }
     }
 
     private fun openMovieDetails(movie: Movies) {
